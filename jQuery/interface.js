552a672979b7c15377ddfc4bec27456d34797ca3
Shendidy <sherif@shendidy.com>
2019-02-09 01:26:12 +0000

    Set AJAX to get current temperature in London and start with it.

--- jQuery/interface.js
+++ jQuery/interface.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   var thermo = new Thermostat();
 
-  setDisplay();
+  start();
 
   $('#upBtn').click(function(){
     var by = document.getElementById('inputSmall').value;
@@ -20,6 +20,7 @@ $(document).ready(function(){
   $('#resetBtn').click(function(){
     thermo.reset();
     document.getElementById('inputSmall').value = null;
+    $('#customSwitch1').prop('checked', true);
     setDisplay();
   });
 
@@ -27,6 +28,16 @@ $(document).ready(function(){
     thermo.psmSwitch();
     setDisplay();
   });
+
+  // This line was written just before trying AJAX!!!
+  function start(){
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      thermo._temperature = data.main.temp;
+      $('#currentTemperature').text(thermo.getCurrentTemperature());
+      $('#currentEnergyUsage').text(thermo.ceu());
+      setColors();
+    });
+  }
 
   function setDisplay(){
     $('#currentTemperature').text(thermo.getCurrentTemperature());
